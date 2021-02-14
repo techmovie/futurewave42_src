e80cb551e4950095b50e751f3e0485d8d223a6bf
Ungigdu <Ungigdu@protonmail.com>
2021-02-14 11:06:43 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div
+    class="bgded overlay"
+    style="
+      background-image: url('/assets/images/backgound/2001_space_odessey_1.jpg');
+    "
+  >
+    <CommonHead></CommonHead>
+  </div>
+  <router-view />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import CommonHead from "./components/CommonHead.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    CommonHead,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/CommonHead.vue
@@ -0,0 +1,43 @@
+<template>
+  <header id="header" class="hoc clear">
+    <div id="logo" class="fl_left" style="height: 100px">
+      <a href="index.html" style="color: white">
+        <img
+          src="/assets/images/logo/logo_futurewave_bianco.png"
+          class="uk-height-1-1"
+        />
+      </a>
+    </div>
+    <nav id="mainav" class="fl_right">
+      <ul class="clear uk-text-center">
+        <li class="active">
+          <router-link to="/">首页<br />Home</router-link>
+        </li>
+        <li>
+          <router-link to="/about">公司介绍<br />About Us</router-link>
+        </li>
+        <li>
+          <a href="#">作品推荐<br />Recommandation</a>
+        </li>
+        <li>
+          <a href="#">相关视频<br />Video</a>
+        </li>
+        <li>
+          <a href="#">联系我们<br />Contect Us</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "CommonHead",
+};
+</script>
+
+<style scoped>
+#mainav ul li {
+  margin-left: 20px;
+}
+</style>
